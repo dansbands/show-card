59072f86def3d0d39fad32e46a47d80eb8aec1b7
dansbands <dansbands@hotmail.com>
2019-05-14 18:46:38 -0400

    header

--- src/App.js
+++ src/App.js
@@ -5,14 +5,31 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Stuff
-          </p>
-          <button onClick={this.handleClick}>
-            Increase Count
-          </button>
-        </header>
+        <div className="App-Container">
+          <div className="header">
+            <div className="header-left">
+              <img
+                src="https://cdn.glitch.com/bf244f65-c514-428a-b4b2-ac2746281e91%2Fposter.jpg?1555596324072"
+                alt="poster"
+                width="65px"
+              />
+            </div>
+            <div className="header-center">
+              <h3>
+                The Phantom of the Opera
+                <span className="light-font"> Tickets </span>
+              </h3>
+              <p className="price">$29.00 - $215.00</p>
+              <a href="#">
+                <img
+                  src="https://cdn.glitch.com/bf244f65-c514-428a-b4b2-ac2746281e91%2Finfo.svg?1555596324183"
+                  alt="i"
+                />
+                <span>Learn More</span>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
